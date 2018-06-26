052994305573e21c7fe487b0ef85a239c49ea320
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2018-06-26 17:29:40 +0700

    Menambahkan Proses Register dan Login

--- app/src/main/java/com/example/budi/pergudangan/HomeActivity.java
+++ app/src/main/java/com/example/budi/pergudangan/HomeActivity.java
@@ -1,8 +1,15 @@
 package com.example.budi.pergudangan;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +19,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.budi.pergudangan.Server.RequestHandler;
+import com.example.budi.pergudangan.Server.Server;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String urlp = Server.showProfil;
+
+    View header;
+    TextView txt_nama, txt_email;
+    CircleImageView user_picture;
+    String idx;
+
+    //untuk login session
+    SharedPreferences sharedpreferences;
+    public static final String TAG_ID = "id_user";
+    public final static String TAG_NAMA = "nama";
+    public final static String TAG_EMAIL = "email";
+    public final static String TAG_FOTO = "foto";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +54,10 @@ public class HomeActivity extends AppCompatActivity
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //untuk mengambil data login session
+        sharedpreferences = getSharedPreferences(Login.my_shared_preferences, Context.MODE_PRIVATE);
+        idx = sharedpreferences.getString(TAG_ID, null);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +76,89 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        header = navigationView.getHeaderView(0);
+        txt_nama = header.findViewById(R.id.nama);
+        txt_email = header.findViewById(R.id.email);
+        user_picture = header.findViewById(R.id.profile_image);
+
+        getUserProfile();
+    }
+
+    private void getUserProfile(){
+        class GetUser extends AsyncTask<Void,Void,String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(HomeActivity.this,"Tunggu",".....",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                showUser(s);
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequestParam(urlp,idx);
+                return s;
+            }
+        }
+        GetUser gu = new GetUser();
+        gu.execute();
+    }
+
+    private void showUser(String json){
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray result = jsonObject.getJSONArray("result");
+            JSONObject c = result.getJSONObject(0);
+            String nama = c.getString(TAG_NAMA);
+            String email = c.getString(TAG_EMAIL);
+            String foto = c.getString(TAG_FOTO);
+
+            txt_nama.setText(nama);
+            txt_email.setText(email);
+            Picasso.with(this).load(foto).placeholder(R.drawable.ic_menu_camera).error(R.drawable.ic_menu_camera).into(user_picture);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void logout(){
+        //Creating an alert dialog to confirm logout
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Anda yakin ingin logout ?");
+        alertDialogBuilder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putBoolean(Login.session_status, false);
+                editor.putString(TAG_ID, null);
+                editor.putString(TAG_NAMA, null);
+                editor.putString(TAG_EMAIL, null);
+                editor.putString(TAG_FOTO, null);
+                editor.commit();
+                Intent intent = new Intent(HomeActivity.this, Login.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+        alertDialogBuilder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+            }
+        });
+
+        //Showing the alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
     }
 
     @Override
@@ -48,7 +167,9 @@ public class HomeActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            // TODO Auto-generated method stub
+            moveTaskToBack(true);
+            //membuat method tombol keluar dari aplikasi
         }
     }
 
---
+++ app/src/main/java/com/example/budi/pergudangan/Server/Server.java
@@ -0,0 +1,6 @@
+package com.example.budi.pergudangan.Server;
+
+public class Server {
+    public static final String URL = "http://wisatajawabarat.online/";
+    public static final String showProfil = "http://wisatajawabarat.online/get_profile.php?id_user=";
+}
