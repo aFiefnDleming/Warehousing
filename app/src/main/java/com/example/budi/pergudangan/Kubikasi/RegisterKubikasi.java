19a629f96f6b7f82a64b2a7b45503b5a69edec61
rrifafauzikomara <rifafauzi6@gmail.com>
2018-07-21 22:12:45 +0700

    penambahan 2 actor login

--- app/src/main/java/com/example/budi/pergudangan/Kubikasi/RegisterKubikasi.java
+++ app/src/main/java/com/example/budi/pergudangan/Kubikasi/RegisterKubikasi.java
@@ -16,7 +16,6 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.example.budi.pergudangan.Packer.LoginPacker;
 import com.example.budi.pergudangan.R;
 import com.example.budi.pergudangan.Server.AppController;
 import com.example.budi.pergudangan.Server.Server;
