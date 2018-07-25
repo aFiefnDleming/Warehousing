4f9fbfddd19fd0ff525f6306651b726018ee492a
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2018-07-25 08:54:56 +0700

    hosting server

--- app/src/main/java/com/example/budi/pergudangan/Server/Server.java
+++ app/src/main/java/com/example/budi/pergudangan/Server/Server.java
@@ -3,10 +3,10 @@ package com.example.budi.pergudangan.Server;
 public class Server {
 
     //URL untuk packer
-    public static final String URL = "http://192.168.100.58/pergudangan/android/";
+    public static final String URL = "http://gudangku.pe.hu/android/";
     public static final String showProfilP = URL + "packer/get_profile.php?id_user=";
     public static final String showProfilK = URL + "kubikasi/get_profile.php?id_user=";
 
     //URL untuk kubikasi
-    public static final String URLK = "http://192.168.100.58/pergudangan/android/kubikasi/";
+    public static final String URLK = "http://gudangku.pe.hu/android/kubikasi/";
 }
