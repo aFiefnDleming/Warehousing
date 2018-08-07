04a328ed47f01902b9c0b0cff7663ab9419147e1
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2018-08-07 10:27:27 +0700

    scaning qr code with change value of status in db

--- app/src/main/java/com/example/budi/pergudangan/Packer/Scanner/Scanner.java
+++ app/src/main/java/com/example/budi/pergudangan/Packer/Scanner/Scanner.java
@@ -1,15 +1,34 @@
 package com.example.budi.pergudangan.Packer.Scanner;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.budi.pergudangan.Kubikasi.Barang.UpdateBarang;
+import com.example.budi.pergudangan.Kubikasi.HomeKubikasi;
 import com.example.budi.pergudangan.Packer.HomePacker;
 import com.example.budi.pergudangan.R;
+import com.example.budi.pergudangan.Server.AppController;
+import com.example.budi.pergudangan.Server.Server;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import info.vividcode.android.zxing.CaptureActivity;
 import info.vividcode.android.zxing.CaptureActivityIntents;
@@ -17,8 +36,17 @@ import info.vividcode.android.zxing.CaptureActivityIntents;
 public class Scanner extends AppCompatActivity {
 
     //pendeklarasian variable dengan class
-    TextView tvScanResult;
+    TextView tv_id;
     Button btScan;
+    String id;
+
+    int success;
+    ConnectivityManager conMgr;
+    private String urlu = Server.URLP + "confirmed.php?id_barang=";
+    private static final String TAG = UpdateBarang.class.getSimpleName();
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    String tag_json_obj = "json_obj_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +54,7 @@ public class Scanner extends AppCompatActivity {
         setContentView(R.layout.activity_scanner);
 
         //merelasikan variable dengan design UI
-        tvScanResult = findViewById(R.id.tv_scanresult);
+        tv_id = findViewById(R.id.tv_id);
         btScan = findViewById(R.id.bt_scan);
 
         //melakukan fungsi scanner QR Code
@@ -66,14 +94,58 @@ public class Scanner extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 0) {
             if (resultCode == Activity.RESULT_OK && data != null) {
-                String value = data.getStringExtra("SCAN_RESULT");
-                tvScanResult.setText(value);
+                id = data.getStringExtra("SCAN_RESULT");
+                conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);{
+                    if (conMgr.getActiveNetworkInfo() != null && conMgr.getActiveNetworkInfo().isAvailable() && conMgr.getActiveNetworkInfo().isConnected()) {
+                        updateBarang(id);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+                    }
+                }
+
             } else if (resultCode == Activity.RESULT_CANCELED) {
-                tvScanResult.setText("Scanning Gagal, mohon coba lagi.");
+                tv_id.setText("Scanning Gagal, mohon coba lagi.");
             }
         } else {
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void updateBarang (final String id) {
+        StringRequest strReq = new StringRequest(Request.Method.POST, urlu+id, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "Update Response: " + response.toString());
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    success = jObj.getInt(TAG_SUCCESS);
+                    // Check for error node in json
+                    if (success == 1) {
+                        Toast.makeText(getApplicationContext(), jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("id_barang", id);
+                return params;
+            }
+        };
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
     }
 
 }
--- app/src/main/java/com/example/budi/pergudangan/Server/Server.java
+++ app/src/main/java/com/example/budi/pergudangan/Server/Server.java
@@ -2,7 +2,7 @@ package com.example.budi.pergudangan.Server;
 
 public class Server {
 
-    //URL untuk packer
+//    //URL untuk packer
     public static final String URL = "http://gudangku.pe.hu/android/";
     public static final String showProfilP = URL + "packer/get_profile.php?id_user=";
     public static final String showProfilK = URL + "kubikasi/get_profile.php?id_user=";
@@ -12,7 +12,7 @@ public class Server {
 
     public static final String URLP = URL + "packer/";
 
-//    //URL untuk packer
+    //URL untuk packer
 //    public static final String URL = "http://192.168.100.69/pergudangan/android/";
 //    public static final String showProfilP = URL + "packer/get_profile.php?id_user=";
 //    public static final String showProfilK = URL + "kubikasi/get_profile.php?id_user=";
